cfef044a935edc8f0fb5ae964e47d797f82428e2
majidgeeks <majidrajput246@gmail.com>
2023-09-05 03:09:36 +0500

    Rendering data using jason place api's for user's and post on react js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BIKE from "./images/bike.webp";
+import posts from "./posts.js";
+import users from "./users.js";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+let obj = { name: "hello world object" };
+let arr1 = ["we", "are", "united"];
+let list = [
+  { name: "Hello World1" },
+  { name: "Hello World2" },
+  { name: "Hello World3" },
+];
+// const complex = [{company: 'XYZ', jobs: ['Javascript,' 'React']}, {company: 'ABC', jobs: ['AngularJs' 'Ionic']}]
+let complex = [
+  { company: "xyz", jobs: ["javascript", "react"] },
+  { company: "ABC", jobs: ["AngularJs", "Ionic"] },
+];
 
 function App() {
+  let name1 = "Hello World";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name1}</h1>
+
+      <h2>MAJID HUSSAIN</h2>
+
+      <img src={BIKE} alt=""></img>
+      <br></br>
+      <br></br>
+
+      <ul>
+        {arr1.map((v, i) => (
+          <b>
+            <li key={i}>{v}</li>
+          </b>
+        ))}
+      </ul>
+      <br></br>
+
+      <p>{obj.name}</p>
+      <br></br>
+
+      <ul>
+        {list.map((v, i) => (
+          <li key={i}>{v.name}</li>
+        ))}
+      </ul>
+      <br></br>
+
+      <ul>
+        {complex.map((v, i) => (
+          <li key={i}>
+            {"Company Name"} {v.company} {"Jobs"}: {v.jobs + ","}
+          </li>
+        ))}
+      </ul>
+      <br></br>
+
+      <div>
+        <ul>
+          
+            <div className="cards">
+              {posts.map((v, i) => (
+                <li className="card card-posts" key={i}>
+                  {" "}
+                  {"id :"} {v.id}
+                  <br></br>
+                  {"title :"} {v.title}
+                  <br></br> {"body :"} {v.body.slice(0, 20)}
+                </li>
+              ))}
+            </div>
+          
+
+          <div className="card-2">
+            {users.map((v, i) => (
+              <li className="card card-users" key={i}>
+               {"name :"} {v.name} <br></br> {"user name :"} {v.username} <br></br> {"email :"} {v.email}
+              </li>
+            ))}
+          </div>
+        </ul>
+      </div>
     </div>
   );
 }
